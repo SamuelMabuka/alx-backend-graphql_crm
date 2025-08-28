5f22ce885d15a9b34c0db2e9d73a1925e488059b
Samuel Mabuka <samuelmabuka@gmail.com>
2025-08-28 22:45:52 +0300

    Endpoint

--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -1,9 +1,11 @@
+# alx_backend_graphql_crm/urls.py
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+import schema  # ✅ import from root, not from alx_backend_graphql_crm.schema
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("admin/", admin.site.urls),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema.schema))),
 ]
